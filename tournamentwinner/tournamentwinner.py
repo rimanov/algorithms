b44c26d8f4d8419e6f369ac747a6a365a6c9eab0
rimanov <rimanov09@gmail.com>
2023-01-02 18:31:21 -0800

    notes

--- tournamentwinner/tournamentwinner.py
+++ tournamentwinner/tournamentwinner.py
@@ -1,6 +1,8 @@
 def tournamentWinner(competitions, results):
     teamPoints=dict()
+    #loop through the competitions array objects
     for i in range(len(competitions)):
+        #check the first match in competitions, and if the winner(1-results[i]) is in the dictionary, add 3 points, if not, set 3 points as base 
         if competitions[i][1 - results[i]] in teamPoints.keys():
             teamPoints[competitions[i][1 - results[i]]] += 3
         else:
